9e2f8720f12a7000551e59867078dd10a60f6150
Bruno Oliveira <brunno.oliver7@gmail.com>
2022-03-15 00:01:36 -0300

    GamesRepository

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -13,18 +13,26 @@ export class GamesRepository implements IGamesRepository {
   }
 
   async findByTitleContaining(param: string): Promise<Game[]> {
-    return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+    return await this.repository
+      .createQueryBuilder('game')
+      .where('game.title ilike :title', { title: `%${param}%` })
+      .getMany()
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return await this.repository.query(`
+      SELECT COUNT(*)
+      FROM games
+    `);
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
-    return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+    const game = await this.repository
+      .createQueryBuilder('game')
+      .innerJoinAndSelect('game.users', 'users')
+      .where('game.id = :id', { id: id })
+      .getOneOrFail();
+
+    return game.users;
   }
 }
